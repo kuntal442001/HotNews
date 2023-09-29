2403326058e829c52b01320df3987db6ce04be5d
Kuntal <117976849+kuntal442001@users.noreply.github.com>
2023-09-29 16:08:26 +0530

    Add files via upload

---
+++ build/precache-manifest.47ebae27a0f7abb054d3e2418cc5f325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ba7536bb23e6170d912",
+    "url": "/HotNews/static/css/main.b48c1176.chunk.css"
+  },
+  {
+    "revision": "9ba7536bb23e6170d912",
+    "url": "/HotNews/static/js/main.9ba7536b.chunk.js"
+  },
+  {
+    "revision": "0f42ad21f20e3536abc0",
+    "url": "/HotNews/static/js/1.0f42ad21.chunk.js"
+  },
+  {
+    "revision": "d4c229e8fad86dc4a32a",
+    "url": "/HotNews/static/js/2.d4c229e8.chunk.js"
+  },
+  {
+    "revision": "be120292dc46d9e5c150",
+    "url": "/HotNews/static/js/runtime~main.be120292.js"
+  },
+  {
+    "revision": "05c337a94fc553558bc56d0bc149960a",
+    "url": "/HotNews/static/media/Loading.05c337a9.gif"
+  },
+  {
+    "revision": "e886ba567a6120492958f45c763ff2c8",
+    "url": "/HotNews/index.html"
+  }
+];
